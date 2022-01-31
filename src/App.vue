929aec5eb54a993e1c73200abc29cc3f82e090ea
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-31 15:37:21 +0100

    Add: discList from API

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,23 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
 
+  },
+  data(){
+    return{
+      discList:[]
+    }
+  },
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((list) =>{
+      this.discList=list.data.response;
+      console.log(list)
+    })
   }
 }
 </script>
