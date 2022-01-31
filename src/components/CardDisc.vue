15abf666b489ea6b99b31391435edcef5b4a40de
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-31 16:12:43 +0100

    Add: CardDisc component

---
+++ src/components/CardDisc.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{disc.title}}</div>
+</template>
+
+<script>
+export default {
+    props:{
+        disc:Object
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/MainBox.vue
+++ src/components/MainBox.vue
@@ -1,9 +1,17 @@
 <template>
-    <div></div>
+    <main>
+        <card-disc v-for="(disc, index) in discList"
+        :key="index"
+        :disc="disc" />
+    </main>
 </template>
 
 <script>
+import CardDisc from './CardDisc.vue'
 export default {
+    components: {
+        CardDisc
+    },
     props:{
         discList:Array
     }
